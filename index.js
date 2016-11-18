326af877fe731e4bbcaa4c7428ea16041c7cdfa8
root <root@nodejs.lan>
2016-11-18 19:24:21 +0300

    sec comm

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express');
+var cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 app.get('/', (req, res, next) => {
 
   const username = typeof(req.query.username)=="undefined" ? "Empty Empty Empty" : req.query.username;
-  const result = /[a-z\d_]+$/gi.exec(username.replace(/\?.+$/gi, ''));
-  res.send(`Query: ${req.originalUrl} <br>-<br>Вывод: @${result[0]}`);
+  const result = /[a-z\d_\-.]+$/gi.exec(username.replace(/\?.+$/gi, '')
+	.replace(/(https?:\/\/[^\/]+\/[^\/]+)\/.+$/gi, '$1'));
+  console.log(`Query: ${req.originalUrl} <br>-<br>Вывод: @${result[0]}`);
+  res.send("@"+result[0]);
 
 });
 
